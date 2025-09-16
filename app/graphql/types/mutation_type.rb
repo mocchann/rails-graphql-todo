7ebe9a510fe85aa1e5f136bb1102098cb1dbec36
もっちゃん <s7056i3056@gmail.com>
2025-09-16 23:11:03 +0900

    feat: deleteメソッドを追加

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_todo, mutation: Mutations::Todo::CreateTodo
     field :update_todo, mutation: Mutations::Todo::UpdateTodo
+    field :delete_todo, mutation: Mutations::Todo::DeleteTodo
   end
 end
