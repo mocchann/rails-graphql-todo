a52ab6e866f37e570ed8c9c0425f174557223b1a
もっちゃん <s7056i3056@gmail.com>
2025-09-29 12:35:09 +0900

    fix: current_userをcontextから取得

--- app/graphql/mobile/mutations/auth/sign_out.rb
+++ app/graphql/mobile/mutations/auth/sign_out.rb
@@ -6,8 +6,8 @@ module Mobile::Mutations::Auth
     field :errors, [ String ], null: false
 
     def resolve
-      if current_user
-        sign_out(current_user)
+      if context[:current_user]
+        sign_out(context[:current_user])
         { success: true, errors: [] }
       else
         { success: false, errors: [ "Not signed in" ] }
